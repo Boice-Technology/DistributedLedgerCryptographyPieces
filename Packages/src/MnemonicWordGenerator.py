957ecc96349999129fafa6c5ade20173de50f83e
DivyPatel9881 <divy9881@gmail.com>
2019-05-21 15:39:57 +0530

    Modified Mnemonic words files.

--- Packages/src/MnemonicWordGenerator.py
+++ Packages/src/MnemonicWordGenerator.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv("vocabwords.csv")
 
 str = "\"" + df["Word"][0] + "\""
-for i in range(1,1024):
+for i in range(1,2048):
 	str = str + ", " + "\"" + df["Word"][i] + "\""
 
 f = open("MnemonicWords.go","w")
