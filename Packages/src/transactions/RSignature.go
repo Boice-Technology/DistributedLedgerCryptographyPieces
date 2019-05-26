16b42979f2d0d295139f37e81dcbcd0b902345e6
DivyPatel9881 <divy9881@gmail.com>
2019-05-26 10:52:46 +0530

    Mod changed to the order of elliptic curve.

--- Packages/src/transactions/RSignature.go
+++ Packages/src/transactions/RSignature.go
@@ -8,8 +8,12 @@ import ("eckg";
 func RSignature(ephePri *big.Int) (string) {
 	x_coor,_ := EphemeralPublicKey(ephePri)
 	r,_ := new(big.Int).SetString(x_coor,10)
-	primef,_ := new(big.Int).SetString(eckg.PrimeF,10)
-	r.Mod(r,primef)
+	fmt.Println(r)
+	n,_ := new(big.Int).SetString(eckg.N,10)
+	fmt.Println(n.Cmp(r))
+	r.Mod(r,n)
+	fmt.Println(r)
 	rStr := fmt.Sprintf("%s",r)
+	fmt.Println(rStr)
 	return rStr
 }
