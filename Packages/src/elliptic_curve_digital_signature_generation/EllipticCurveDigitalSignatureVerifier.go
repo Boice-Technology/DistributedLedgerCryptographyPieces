d4bded8dfe8e42351437b71e08973cca987534d3
DivyPatel9881 <divy9881@gmail.com>
2019-09-28 23:31:15 +0530

    Bitcoin book deleted

---
+++ Packages/src/elliptic_curve_digital_signature_generation/EllipticCurveDigitalSignatureVerifier.go
@@ -0,0 +1,11 @@
+// Verify the signature is legit or not
+
+package elliptic_curve_digital_signature_generation
+
+import ("elliptic_curve_key_generation";
+		)
+		
+func EllipticCurveDigitalSignatureVerifier(RSignatueComponent string, SSignatureComponent string, publicKeyX string, publicKeyY string, message string) bool {
+	
+}
+
